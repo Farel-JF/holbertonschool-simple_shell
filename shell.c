1d55431db09630ea75863d4a430875297f84ad05
antonin-crypto <akamis53@gmail.com>
2024-04-15 13:18:18 +0000

    creation file shell

---
+++ shell.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAX_COMMAND_LENGTH 100
+
+void run_command(char *command) {
+    if (system(command) == -1) {
+        perror("Error executing command");
+    }
+}
+
+void list_directory_contents() {
+    run_command("ls -l");
+}
+
+int main() {
+    char command[MAX_COMMAND_LENGTH];
+
+    while (1) {
+        printf("$ ");
+        if (fgets(command, sizeof(command), stdin) == NULL) {
+            break;
+        }
+
+        // Remove trailing newline
+        command[strcspn(command, "\n")] = 0;
+
+        if (strcmp(command, "exit") == 0) {
+            break;
+        }
+
+        if (strcmp(command, "l") == 0) {
+            list_directory_contents();
+            continue;
+        }
+
+        run_command(command);
+    }
+
+    return 0;
+}
