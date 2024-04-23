71a1f0053761e898c25d4a7396eaf30375bc5b10
antonin-crypto <akamis53@gmail.com>
2024-04-23 15:12:06 +0200

    modified simple_shell non interatif

--- simple_shell.c
+++ simple_shell.c
@@ -4,36 +4,15 @@ int main(int ac, char *av[], char **env)
 {
     int nb_cmd = 1, status = 0;
     char input[MAX_INPUT_LENGTH];
+    (void) av;
 
-    /* Vérifier le mode interactif ou non */
-    if (!isatty(STDIN_FILENO))
-    {
-        FILE *file = fopen(av[0], "r");
-        if (file == NULL)
-        {
-            fprintf(stderr, "Erreur lors de l'ouverture du fichier.\n");
-            return (EXIT_FAILURE);
-        }
-
-        /* Lire les commandes à partir du fichier et les exécuter */
-        while (fgets(input, MAX_INPUT_LENGTH, file) != NULL)
-        {
-            /*Remove trailing newline character*/
-            input[strcspn(input, "\n")] = '\0';
-            /*Execute the command*/
-            execute_command(input);
-            nb_cmd++;
-        }
-
-        fclose(file);
-        return (status);
-    }
 
     /* Mode interactif */
     while (1)
      {
+        if (isatty(STDIN_FILENO))
         /* Afficher le prompt et lire l'entrée utilisateur */
-        printf("cisfun$ ");
+            printf("cisfun$ ");
         if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL)
         {
             if (feof(stdin))
